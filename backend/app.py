73334a13f4bdc2e5b80b6e83c83eb4e39d7b6eb7
prudhvi9121 <prudhvikarri9121@gmail.com>
2025-07-09 17:25:17 +0530

    Fix: bind to 0.0.0.0

--- backend/app.py
+++ backend/app.py
@@ -2,43 +2,34 @@ from flask import Flask, request, jsonify, send_from_directory
 from db import collection
 from utils import format_message
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='')
-
-@app.route("/", methods=["GET"])
-def index():
-    return "Webhook Receiver Running!"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     event_type = request.headers.get("X-GitHub-Event")
     payload = request.get_json()
-
     data = format_message(event_type, payload)
     if data:
         collection.insert_one(data)
         return jsonify({"status": "success", "data": data}), 201
-    else:
-        return jsonify({"status": "ignored", "reason": "unsupported event or action"}), 400
+    return jsonify({"status": "ignored"}), 400
 
 @app.route("/events", methods=["GET"])
 def get_events():
     latest = list(collection.find({}, {"_id": 0}).sort("timestamp", -1).limit(10))
     return jsonify(latest)
 
-# Serve index.html
 @app.route("/ui")
-def serve_frontend():
+def serve_ui():
     return send_from_directory(app.static_folder, "index.html")
 
-# Serve JS and CSS
 @app.route("/<path:path>")
-def static_files(path):
+def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
     port = int(os.getenv("PORT", 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True,host="0.0.0.0" ,port=port)
